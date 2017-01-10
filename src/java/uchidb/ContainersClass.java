18121da9bac7007f3bc9709ff57efbdb8ea249a8
Connor Soltas <csoltas@gmail.com>
2017-01-10 13:39:48 -0600

    edited HW0Runner, added ContainersClass.java

---
+++ src/java/uchidb/ContainersClass.java
@@ -0,0 +1,55 @@
+package uchidb;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ContainersClass<T, S> implements Containers<T, S>
+{
+	private Map<S,T> contmap;
+
+	public Set<T> initSet(T[] tArray) {
+		Set<T> set = new HashSet<T>(tArray);
+		return set;
+	}
+	
+	public List<T> initList(T[] tArray) {
+		List<T> list = new ArrayList<T>(tArray);
+		return list;
+	}
+
+	public Map<S,T> initEmptyMap() {
+		Map<S,T> map = new HashMap<S,T>();
+		return map;
+	}
+	
+	public void storeMap(Map<S,T> mapToStoreInClass) {
+		this.contmap = mapToStoreInClass;
+	}
+
+	public boolean addToMap(S key, T value, boolean overwriteExistingKey) {
+		if (!this.contmap.containsKey(key) || overwriteExistingKey) {
+			this.contmap.put(key, value);
+			return true;
+		}
+		return false;
+	}
+	
+	public T getValueFromMap(S key) {
+		T ret = this.contmap.get(key);
+		return ret;
+	}
+	
+	// An overloaded function to get a value from the store/local map, but with a default value
+	// if the key is not present in the map.
+	public T getValueFromMap(S key, T defaultValue) {
+		T ret = this.contmap.get(key);
+		if (ret != null)
+			return ret;
+		return defaultValue;
+	}
+}
--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -7,14 +7,14 @@ package uchidb;
 public class HW0Runner {
 
 	//TODO you likely will need to add member variable
+	private static HW0Runner hw0runner = new HW0Runner();
+	private HW0Runner() {}
 	
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
-		// TODO fix this function
-		return null;
+		return hw0runner;
 	}
-
 
 	public static void main(String[] args){
 		
